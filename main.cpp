946dac1fc53418d1818cbfc5007315d6b3e454b9
mr-eyes <mabuelanin@gmail.com>
2022-08-10 20:50:29 +0200

    change parser

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@
 #include <ctime>
 #include "combination.hpp"
 #include<omp.h>
-#include "RSJparser.tcc"
 #include <glob.h>
 #include <string>
 #include <stdexcept>
+#include "cpp_json.h"
+#include "zstr.hpp"
 
 using boost::adaptors::transformed;
 using boost::algorithm::join;
 using namespace std;
 using namespace phmap;
-using JSON = RSJresource;
 
 
 #define SIGS_LOAD_THREADS 64
@@ -27,63 +27,18 @@ using JSON = RSJresource;
 
 typedef std::chrono::high_resolution_clock Time;
 
-
-inline uint64_t unrolled(std::string const& value) {
-    uint64_t result = 0;
-
-    size_t const length = value.size();
-    switch (length) {
-    case 20:    result += (value[length - 20] - '0') * 10000000000000000000ULL;
-    case 19:    result += (value[length - 19] - '0') * 1000000000000000000ULL;
-    case 18:    result += (value[length - 18] - '0') * 100000000000000000ULL;
-    case 17:    result += (value[length - 17] - '0') * 10000000000000000ULL;
-    case 16:    result += (value[length - 16] - '0') * 1000000000000000ULL;
-    case 15:    result += (value[length - 15] - '0') * 100000000000000ULL;
-    case 14:    result += (value[length - 14] - '0') * 10000000000000ULL;
-    case 13:    result += (value[length - 13] - '0') * 1000000000000ULL;
-    case 12:    result += (value[length - 12] - '0') * 100000000000ULL;
-    case 11:    result += (value[length - 11] - '0') * 10000000000ULL;
-    case 10:    result += (value[length - 10] - '0') * 1000000000ULL;
-    case  9:    result += (value[length - 9] - '0') * 100000000ULL;
-    case  8:    result += (value[length - 8] - '0') * 10000000ULL;
-    case  7:    result += (value[length - 7] - '0') * 1000000ULL;
-    case  6:    result += (value[length - 6] - '0') * 100000ULL;
-    case  5:    result += (value[length - 5] - '0') * 10000ULL;
-    case  4:    result += (value[length - 4] - '0') * 1000ULL;
-    case  3:    result += (value[length - 3] - '0') * 100ULL;
-    case  2:    result += (value[length - 2] - '0') * 10ULL;
-    case  1:    result += (value[length - 1] - '0');
-    }
-    return result;
-}
-
 using SIGS_MAP = parallel_flat_hash_map<std::string, phmap::flat_hash_set<uint64_t>,
     phmap::priv::hash_default_hash<std::string>,
     phmap::priv::hash_default_eq<std::string>,
-    std::allocator<std::pair<const std::string,
-    phmap::flat_hash_set<uint64_t>>>,
+    std::allocator<std::pair<const std::string, phmap::flat_hash_set<uint64_t>>>,
     12
-    >;
+>;
 
 using PAIRWISE_MAP = parallel_flat_hash_map<std::pair<uint32_t, uint32_t>,
     float, boost::hash<pair<uint32_t, uint32_t>>,
     std::equal_to<std::pair<uint32_t, uint32_t>>,
     std::allocator<std::pair<const std::pair<uint32_t, uint32_t>, float>>,
     12, std::mutex>;
-
-
-template <typename T>
-void ascending(T& dFirst, T& dSecond)
-{
-    if (dFirst > dSecond)
-        std::swap(dFirst, dSecond);
-}
-
-template<>
-uint64_t RSJresource::as<uint64_t>(const uint64_t& def) {
-    if (!exists()) return (0); // required
-    return (unrolled(data)); // example
-}
 
 
 std::vector<std::string> glob2(const std::string& pattern) {
@@ -102,11 +57,9 @@ std::vector<std::string> glob2(const std::string& pattern) {
         throw std::runtime_error(ss.str());
     }
 
-    // collect all the filenames into a std::list<std::string>
     vector<string> filenames;
-    for (size_t i = 0; i < glob_result.gl_pathc; ++i) {
+    for (size_t i = 0; i < glob_result.gl_pathc; ++i)
         filenames.push_back(string(glob_result.gl_pathv[i]));
-    }
 
     // cleanup
     globfree(&glob_result);
@@ -159,19 +112,21 @@ int main(int argc, char** argv) {
         ++sigIdx;
         cout << "\r" << "loading " << sigIdx << "/" << N;
         flat_hash_set<uint64_t> tmp_hashes;
-        std::ifstream sig_stream(sig_path);
-        JSON sig(sig_stream);
-        int number_of_sub_sigs = sig[0]["signatures"].size();
-        for (int i = 0; i < number_of_sub_sigs; i++) {
-            int current_kSize = sig[0]["signatures"][i]["ksize"].as<int>();
-            auto loaded_sig_it = sig[0]["signatures"][i]["mins"].as_array().begin();
-            if (current_kSize == kSize) {
-                while (loaded_sig_it != sig[0]["signatures"][i]["mins"].as_array().end()) {
-                    tmp_hashes.insert(loaded_sig_it->as<uint64_t>());
-                    loaded_sig_it++;
+        zstr::ifstream sig_stream(sig_path);
+        json::value json = json::parse(sig_stream);
+        auto sourmash_sig = json[0]["signatures"];
+        const json::array& sig_array = as_array(sourmash_sig);
+        for (auto it = sig_array.begin(); it != sig_array.end(); ++it) {
+            const json::value& v = *it;
+            if (v["ksize"] == kSize) {
+                const json::array& mins = as_array(v["mins"]);
+                auto mins_it = mins.begin();
+                while (mins_it != mins.end()) {
+                    tmp_hashes.insert(json::to_number<uint64_t>(*mins_it));
+                    mins_it++;
                 }
-                break;
             }
+            break;
         }
         sig_to_hashes.insert(pair(sig_names[sigIdx], tmp_hashes));
     }
